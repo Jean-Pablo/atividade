f5f394230573b1bfcbe20828f910b08f0e071a7b
Jean-Pablo <jean.oliveira.rodrigues@escola.pr.gov.br>
2025-03-12 12:26:05 -0300

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main(){
+    
+    int ano;
+    scanf("%d", ano);
+    if (((ano % 4 == 0) && !(ano % 100 == 0)) || (ano % 400 == 0)){
+        printf("bissexto");
+    } else {
+        printf("naao eh bissexto");
+    }
+    return 0;
+}
